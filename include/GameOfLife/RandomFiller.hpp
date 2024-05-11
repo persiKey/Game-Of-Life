1eb49e2312a45b2cec438e3d92fe196fbd51718c
persiKey <parhomchuk308@gmail.com>
2024-05-11 16:09:50 +0300

    feature/benchmark added seed to Random Filler for more precise benchmark

--- include/GameOfLife/RandomFiller.hpp
+++ include/GameOfLife/RandomFiller.hpp
@@ -13,10 +13,11 @@ namespace GameOfLife
 class RandomFiller : public ICheckBoardFiller
 {
 public:
-    RandomFiller(double alpha);
+    RandomFiller(double alpha,int seed = 0);
     void Fill(std::shared_ptr<ICheckBoard> checkboard) override;
 private:
     double m_alpha;
+    int m_seed = 0;
 };
 
 } // GameOfLife
